ade2c57dd442cd14953bf2c9ade7c4f9be985d79
PrathmeshKanekar <prathmeshkanekar2002@gmail.com>
2023-07-23 23:19:34 +0530

    first commit

---
+++ AIDMOSBackend/Controllers/AccLedgersController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AIDMOSBackend.Context;
+using AIDMOS_BackEnd.Models;
+
+namespace AIDMOSBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccLedgersController : ControllerBase
+    {
+        private readonly ProjectDBContext _context;
+
+        public AccLedgersController(ProjectDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AccLedgers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AccLedger>>> GetAccLedgers()
+        {
+          if (_context.AccLedgers == null)
+          {
+              return NotFound();
+          }
+            return await _context.AccLedgers.ToListAsync();
+        }
+
+        // GET: api/AccLedgers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AccLedger>> GetAccLedger(decimal id)
+        {
+          if (_context.AccLedgers == null)
+          {
+              return NotFound();
+          }
+            var accLedger = await _context.AccLedgers.FindAsync(id);
+
+            if (accLedger == null)
+            {
+                return NotFound();
+            }
+
+            return accLedger;
+        }
+
+        // PUT: api/AccLedgers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAccLedger(decimal id, AccLedger accLedger)
+        {
+            if (id != accLedger.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(accLedger).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AccLedgerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/AccLedgers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<AccLedger>> PostAccLedger(AccLedger accLedger)
+        {
+          if (_context.AccLedgers == null)
+          {
+              return Problem("Entity set 'ProjectDBContext.AccLedgers'  is null.");
+          }
+            _context.AccLedgers.Add(accLedger);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (AccLedgerExists(accLedger.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetAccLedger", new { id = accLedger.Id }, accLedger);
+        }
+
+        // DELETE: api/AccLedgers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAccLedger(decimal id)
+        {
+            if (_context.AccLedgers == null)
+            {
+                return NotFound();
+            }
+            var accLedger = await _context.AccLedgers.FindAsync(id);
+            if (accLedger == null)
+            {
+                return NotFound();
+            }
+
+            _context.AccLedgers.Remove(accLedger);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AccLedgerExists(decimal id)
+        {
+            return (_context.AccLedgers?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ AIDMOSBackend/Models/AccLedger.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace AIDMOS_BackEnd.Models;
+
+[Table("acc_ledgers")]
+public partial class AccLedger
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    [Column("id", TypeName = "numeric(18, 0)")]
+    public decimal Id { get; set; }
+
+    [Column("businessid", TypeName = "numeric(18, 0)")]
+    public decimal? Businessid { get; set; }
+
+    [Column("groupid", TypeName = "numeric(18, 0)")]
+    public decimal? Groupid { get; set; }
+
+    [Column("name")]
+    public string? Name { get; set; }
+
+    [Column("address")]
+    public string? Address { get; set; }
+
+    [Column("pincode")]
+    [StringLength(50)]
+    public string? Pincode { get; set; }
+
+    [Column("villageid", TypeName = "numeric(18, 0)")]
+    public decimal? Villageid { get; set; }
+
+    [Column("email")]
+    public string? Email { get; set; }
+
+    [Column("mobileno")]
+    [StringLength(50)]
+    public string? Mobileno { get; set; }
+
+    [Column("altmobileno")]
+    [StringLength(50)]
+    public string? Altmobileno { get; set; }
+
+    [Column("gstno")]
+    [StringLength(50)]
+    public string? Gstno { get; set; }
+
+    [Column("panno")]
+    [StringLength(50)]
+    public string? Panno { get; set; }
+
+    [Column("adharno")]
+    [StringLength(50)]
+    public string? Adharno { get; set; }
+
+    [Column("status")]
+    [StringLength(50)]
+    public string? Status { get; set; }
+
+    [Column("createdby", TypeName = "numeric(18, 0)")]
+    public decimal? Createdby { get; set; }
+
+    [Column("createdon", TypeName = "datetime")]
+    public DateTime? Createdon { get; set; }
+
+    [Column("updatedby", TypeName = "numeric(18, 0)")]
+    public decimal? Updatedby { get; set; }
+
+    [Column("updatedon", TypeName = "datetime")]
+    public DateTime? Updatedon { get; set; }
+
+    [Column("deletedby", TypeName = "numeric(18, 0)")]
+    public decimal? Deletedby { get; set; }
+
+    [Column("deletedon", TypeName = "datetime")]
+    public DateTime? Deletedon { get; set; }
+
+    [Column("accountno")]
+    public string? Accountno { get; set; }
+
+    [Column("ifsccode")]
+    public string? Ifsccode { get; set; }
+
+    [Column("branch")]
+    public string? Branch { get; set; }
+
+    [Column("openingbalance", TypeName = "numeric(18, 2)")]
+    public decimal? Openingbalance { get; set; }
+
+    [Column("contactperson")]
+    [StringLength(500)]
+    public string? Contactperson { get; set; }
+
+    [Column("creditortype")]
+    [StringLength(50)]
+    public string? Creditortype { get; set; }
+
+}
